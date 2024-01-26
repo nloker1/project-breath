f36b2376dd0fcac0ed6b4f2da986bf88d6660f0e
Nate Loker <nloker1@gmail.com>
2024-01-26 15:20:44 -0800

    New build

--- frontend-pb/src/EducationContent.js
+++ frontend-pb/src/EducationContent.js
@@ -2,7 +2,7 @@ const EducationContent = {
   article1: {
     title: 'The Biochemistry of a Breath',
     date: '1/25/24',
-    mdLink: '/education/breath-biochemistry-overview.md'
+    mdLink: '/education-content/breath-biochemistry-overview.md'
   },
 
   // Add more articles or content here
